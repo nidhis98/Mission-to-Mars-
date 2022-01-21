0a48ac5ae78e4a26da234a2d0fa056baee9a04c7
Nidhi Singh <nidhi_singh98@hotmail.com>
2022-01-20 20:52:18 -0500

    Adding changes to files.

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 app = Flask(__name__)
 
-
-# In[2]:
-
-
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-
-# In[3]:
-
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
 
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
-   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   mars.update_many({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 if __name__ == "__main__":
    app.run()
